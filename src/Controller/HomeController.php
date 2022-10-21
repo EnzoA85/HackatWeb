c3839bbc875f7e051e13c8ae65b563f48083556e
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2022-10-21 16:59:30 +0200

    page l'agence v1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,12 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/HackatAgence', name: 'app_agence')]
+    public function HackatAgence(): Response
+    {
+        return $this->render('home/hackatagence.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
 }
