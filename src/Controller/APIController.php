bcfc3bc70e192fec409a5befd89871e667902474
Damien Boudero <damien.boudero@laposte.net>
2022-11-14 14:42:50 +0100

    api pour les hackathons retournant la liste des hackathons en json fonctionne

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -9,19 +9,26 @@ use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Hackathon;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/*use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;*/
+
+
 class APIController extends AbstractController
 {
     //api retournant un tableau de tout les hackathons
     #[Route('/api', name: 'app_apiLesHackathons')]
-    public function apiLesHackathons(ManagerRegistry $doctrine): Response
+    public function apiLesHackathons(ManagerRegistry $doctrine): JsonResponse
     {
         
         $repository = $doctrine->getRepository(Hackathon::class);
         $lesHackathons = $repository->findAll();
         $tableauHackathons=[];
+
         //Boucle pour récupérer les informations de chaque hackathon
         foreach($lesHackathons as $leHackathon){
-            $tableauHackathon[]=['hackathon'[
+            $tableauHackathons[] = [
                     'id'=>$leHackathon->getId(),
                     'dateDebut'=>$leHackathon->getDateDebut(),
                     'lieu'=>$leHackathon->getLieu(),
@@ -36,8 +43,9 @@ class APIController extends AbstractController
                     'dateFin'=>$leHackathon->getDateFin(),
                     'heureFin'=>$leHackathon->getHeureFin(),
                     'dateLimite'=>$leHackathon->getDateLimite()
-            ]];
-            }      
+            ];
+            }   
+  
         return new JsonResponse($tableauHackathons);
     }
 
