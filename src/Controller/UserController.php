635b527f789384fe8cb01ec05a4f62a48edd05f5
Gregoire LE BRAS <lebras.gregoire@gmail.com>
2022-11-14 14:35:56 +0100

    suppression route inutile user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -43,32 +43,4 @@ class UserController extends AbstractController
             }
         }
     }
-
-    #[Route('/ValidationSignUp', name: 'app_Validercreercompte')]
-    public function validercreercompte(ManagerRegistry $doctrine)
-    {
-        $nom=$_POST["nom"];
-        $prenom=$_POST["prenom"];
-        $email=$_POST["email"];
-        $dateNaissance=$_POST["dateNaissance"];
-        $tel=$_POST["tel"];
-        $lienportfolio=$_POST["portfolio"];
-        $mdp=$_POST["mdp"];
-        $confmdp=$_POST["confmdp"];
-        if($mdp==$confmdp)
-        {
-            $user = new Utilisateur();
-            $user->setNom($nom);
-            $user->setPrenom($prenom);
-            $user->setMail($email);
-            $user->setDateNaissance(new \DateTime($dateNaissance));
-            $user->setTel($tel);
-            $user->setLienPortfolio($lienportfolio);
-            $user->setMdp(password_hash($mdp,PASSWORD_BCRYPT));
-            $entityManager = $doctrine->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-            return $this->render('user/ValidationSignUp.html.twig', ['nom' => $nom, 'prenom' => $prenom, 'email'=>$email,'dateNaissance'=>$dateNaissance,'tel'=>$tel,'portfolio'=>$lienportfolio,]); 
-        }
-    }
 }
