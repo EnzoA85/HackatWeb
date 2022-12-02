fbe31c03cabbb30de753ee28976fcad3c9781eb7
Damien BOUDERO <DBOUDERO>
2022-12-02 15:04:31 +0100

    modif du modal et ajout du controlleur login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('home/index.html.twig', [
+            'last_username' => 'lastUsername',
+        ]);
+    }
+}
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -131,7 +131,6 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
