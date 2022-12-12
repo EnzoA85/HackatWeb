8f6e9dfca8656b25d140047be09db0d69528716e
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2022-12-12 11:07:30 +0100

    formulaire symfony

---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('mail')
+            ->add('dateNaissance')
+            ->add('lienPortfolio')
+            ->add('mdp',PasswordType::class,['label' => 'Mot de passe'])
+            ->add('tel',TypeTextType::class, ['label' => 'Téléphone'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
