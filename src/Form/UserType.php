faf218da8442e834174ab459f191f0b34c714ad2
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2023-02-27 10:45:15 +0100

    comment pattern password

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -29,7 +29,7 @@ class UserType extends AbstractType
             ->add('mdp', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les mots de passe ne sont pas identiques.',
-                'options' => ['attr' => ['class' => 'password-field','pattern'=>'[a-z]{1,}[A-Z]{1,}[0-9]{1,}','minlength'=>12]],
+                // 'options' => ['attr' => ['class' => 'password-field','pattern'=>'[a-z]{1,}[A-Z]{1,}[0-9]{1,}','minlength'=>12]],
                 'required' => true,
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Confirmer le mot de passe'],
