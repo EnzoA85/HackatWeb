71340037c600be891864249ca61c1f703a20f4fa
Damien BOUDERO <DBOUDERO>
2023-03-13 12:04:44 +0100

    Ajout de la route /favorisHackathon/ id pour ajouter un favoris à lutilisateur connecté

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Hackathon;
 use App\Entity\Inscription;
+use App\Entity\Utilisateur;
 use Doctrine\Persistence\ManagerRegistry;
 
 class InscriptionController extends AbstractController
@@ -32,12 +33,30 @@ class InscriptionController extends AbstractController
             $inscription->setTexteLibre($_POST["texteLibre"]);
             $inscription->setDateInscription(new \DateTime(date_default_timezone_get()));
 
-            $inscription->setHackathon($doctrine->getRepository(Hackathon::class)->findOneBy(['id' => $_POST["hackathon"]])); // on cherche un hackathon par rapport à l'id entrer
-            $inscription->setUtilisateur($this->getUser()); // on cherche un utilisateur par rapport à l'utilisateur connecter
+            $inscription->setHackathon($doctrine->getRepository(Hackathon::class)->findOneBy(['id' => $_POST["hackathon"]])); // on cherche un hackathon par rapport à l'id entrée
+            $inscription->setUtilisateur($this->getUser()); // on cherche un utilisateur par rapport à l'utilisateur connecté
             
             $entityManager = $doctrine->getManager();
             $entityManager->persist($inscription);
             $entityManager->flush();
             return $this->render('home/index.html.twig');
     }
+
+    #[Route('/favoriHackathon/{id}', name: 'app_MettreFavoris')]
+    public function mettreFavoriHackathon($id, ManagerRegistry $doctrine)
+    {
+        #recupération de l'utilisateur
+        $Utilisateur = $this->getUser();
+        #recupération du hackathon
+        $Hackathon = $doctrine->getRepository(Hackathon::class)->findOneBy(array('id' => $id));
+        
+        #Mettre le hackathon en favoris
+        $Utilisateur->addFavori($Hackathon);
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($Utilisateur);
+        $entityManager->flush();
+
+        return $this->render('/hackathons');
+    }
+
 }
